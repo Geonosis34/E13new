ba0ec0a3336b8c3fba273e37376d6c9c7f30213f
Geonosis <shishkin.vlad92@yandex.ru>
2023-01-13 22:34:45 +0300

    try first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Hello World 2!!!!!!</h1>
+        </div>
+        );
+    }
+}
+
+export default App;
+
+if (module.hot) {
+    module.hot.accept()
+}
